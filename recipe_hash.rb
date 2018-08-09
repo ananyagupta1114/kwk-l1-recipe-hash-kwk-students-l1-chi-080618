30b14407190511ed1fe888a4c0d991f2e6fca2fb
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-09 15:16:44 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -9,8 +9,9 @@ perfect_10_recipe = {
 :mini_dark_chocolate_chips => "3/4 cup",
 :olive_oil => "1/2 cup",
 :agave => "1/4 cup"
+}
 
-puts perfect_10_recipe
+perfect_10_recipe
 
 #code your solutions here
 
